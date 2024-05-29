73f8b57a1eeb2f7a7317d87661a1eadcd83bc3fd
paragonrubio <paragonrubio@gmail.com>
2024-05-29 15:52:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,31 +12,10 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
-st.write(pd.read_csv("imdb-movies-dataset.csv").columns.tolist())
 
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+start_color, end_color = st.select_slider(
+    "Select a range of color wavelength",
+    options=["red", "orange", "yellow", "green", "blue", "indigo", "violet"],
+    value=("red", "blue"))
+st.write("You selected wavelengths between", start_color, "and", end_color)
