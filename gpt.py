b631f00256a031a9f7b4b6f83cde06ace9cc2fff
paragonrubio <paragonrubio@gmail.com>
2024-05-29 17:28:24 +0200

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -30,4 +30,4 @@ if prompt := st.chat_input("What is up?"):
             stream=True,
         )
         response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    #st.session_state.messages.append({"role": "assistant", "content": response})
